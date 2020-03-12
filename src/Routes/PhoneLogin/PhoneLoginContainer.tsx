c993b6b62738040cb2f7d0d66fac1ced8ae5e6f8
Alex Levanov <ilewanof@gmail.com>
2020-03-12 23:49:50 +0400

    Add inputs and typescript

--- src/Routes/PhoneLogin/PhoneLoginContainer.tsx
+++ src/Routes/PhoneLogin/PhoneLoginContainer.tsx
@@ -1,10 +1,42 @@
 import React from 'react';
+import { RouteComponentProps } from 'react-router-dom';
 import PhoneLoginPresenter from './PhoneLoginPresenter';
 
-class PhoneLoginContainer extends React.Component {
+interface IState {
+  countryCode: string;
+  phoneNumber: string;
+}
+
+class PhoneLoginContainer extends React.Component<RouteComponentProps<IState>> {
+  public state = {
+    countryCode: '+7',
+    phoneNumber: ''
+  };
   public render() {
-    return <PhoneLoginPresenter />;
+    return (
+      <PhoneLoginPresenter
+        onSubmit={this.onSubmit}
+        {...this.state}
+        onInputChange={this.onInputChange}
+      />
+    );
   }
+
+  public onInputChange: React.ChangeEventHandler<
+    HTMLInputElement | HTMLSelectElement
+  > = event => {
+    const {
+      target: { name, value }
+    } = event;
+    this.setState({
+      [name]: value
+    } as any);
+  };
+
+  public onSubmit: React.FormEventHandler<HTMLFormElement> = event => {
+    event.preventDefault();
+    const { countryCode, phoneNumber } = this.state;
+  };
 }
 
 export default PhoneLoginContainer;
