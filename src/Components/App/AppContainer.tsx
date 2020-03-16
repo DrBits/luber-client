6b227df58bf55ac83fdea7070553f88530cb234c
Alex Levanov <ilewanof@gmail.com>
2020-03-17 00:20:29 +0400

    Add react-toastify

--- src/Components/App/AppContainer.tsx
+++ src/Components/App/AppContainer.tsx
@@ -1,16 +1,22 @@
 import React from 'react';
+import { ToastContainer } from 'react-toastify';
 import { useQuery } from '@apollo/react-hooks';
 import AppPresenter from './AppPresenter';
 import { IS_LOGGED_IN } from './AppQuery';
 import { ThemeProvider } from '../../typed-components';
 import theme from '../../theme';
 
+import 'react-toastify/dist/ReactToastify.min.css';
+
 const AppContainer = () => {
   const { data } = useQuery(IS_LOGGED_IN);
   return (
-    <ThemeProvider theme={theme}>
-      <AppPresenter isLoggedIn={data.auth.isLoggedIn} />
-    </ThemeProvider>
+    <>
+      <ThemeProvider theme={theme}>
+        <AppPresenter isLoggedIn={data.auth.isLoggedIn} />
+      </ThemeProvider>
+      <ToastContainer draggable position={'bottom-center'} />
+    </>
   );
 };
 
